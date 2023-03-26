5821649720df0722400c503cf7456eb5a591641b
unknown <berimecdanil963@gmail.com>
2023-03-26 22:15:17 +0300

    newproject

--- js/script.js
+++ js/script.js
@@ -1,52 +1,170 @@
 let container = document.querySelector('.container')
+let patron = document.querySelector('.patron')
+let patron2 = document.querySelector('.patron2')
+let patron3 = document.querySelector('.patron3')
 
-let dogX = 0;
-
-let dogY = 0;
-
-const renderEl = () => {
-   const patron = `<img src="/img/03.jpg" alt="patron" class="pespatron" 
-   style="position:absolute;top:${dogX}px,left:${dogY}px">`
-
-   container.innerHTML = patron
-}
+patron.style.backgroundImage = "url('./img/01.png')";
+patron2.style.backgroundImage = "url('./img/02.png')";
+patron3.style.backgroundImage = "url('./img/03.png')";
 
 
+// Початкова координата першого патрона
+let patronTop = 15
+let patronLeft = 15
 
-container.addEventListener('click', (ev) => {
-   console.log(ev);
-   if (ev.target.alt != 'patron') { return }
+patron.style.top = patronTop + 'px'
+patron.style.left = patronLeft + 'px'
+// ---------------------------
 
-   let mouseX = dogX;
-   let mouseY = dogY;
+let step = 15;
 
-   let centerUp = mouseX + 200 / 2;
-   let centerLeft = mouseY + 200 / 2;
+patron.addEventListener('click', (ev) => {
+   let guestX = ev.pageX
+   let guestY = ev.pageY
 
-   let rightUp = mouseX + 200;
-   let leftDown = mouseY + 200;
+   let patronX = guestX - patron.getBoundingClientRect().left;
+   let patronY = guestY - patron.getBoundingClientRect().top;
+   console.log('patronX:', patronX)
+   console.log('patronY:', patronY)
 
-   dogX = +50 + 'px';
-   dogY = +40 + 'px';
+   let patXst = parseInt(patron.style.left)
+   let patYst = parseInt(patron.style.top);
+   console.log('patXst:', patXst)
+   console.log('patYst:', patYst)
 
-
-   let Xtarget = ev.clientX;
-   let Ytarget = ev.clientY;
-
-   console.log('X', Xtarget, 'Y', Ytarget);
-
-   if ((mouseX <= Xtarget) && (Xtarget <= centerUp) && (mouseY <= Ytarget) && (Ytarget <= centerLeft)) {
-      console.log('Клік верхній лівий кут')
+   if ((patronX < 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst - step
+      console.log('Верхній лівий кут')
    }
-   if ((centerUp <= Xtarget) && (Xtarget <= rightUp) && (mouseY <= Ytarget) && (Ytarget <= centerLeft)) {
-      console.log('Клік на верхній правий кут')
+   if ((patronX > 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst + step
+      console.log('Нижній правий кут')
    }
-   if ((mouseX <= Xtarget) && (Xtarget <= centerUp) && (centerLeft <= Ytarget) && (Ytarget <= leftDown)) {
-      console.log('Клік на нижній лівий кут')
+   if ((patronX > 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst + step
+      console.log('Верхній правий кут')
    }
-   if ((centerUp <= Xtarget) && (Xtarget <= rightUp) && (centerLeft <= Ytarget) && (Ytarget <= leftDown)) {
-      console.log('Клік на нижній правий кут')
+   if ((patronX < 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst - step
+      console.log('Нижній лівий кут')
    }
+
+   patronTop = guestX
+   patronLeft = guestY
+
+   patron.style.top = guestX + 'px'
+   patron.style.left = guestY + 'px'
+
 })
 
-renderEl()
+// Початкова координата другого патрона
+let patronTop2 = 405
+let patronLeft2 = 405
+
+patron2.style.top = patronTop2 + 'px'
+patron2.style.left = patronLeft2 + 'px'
+// ---------------------------
+
+let step2 = 15;
+
+patron2.addEventListener('click', (ev) => {
+   let guestX = ev.pageX
+   let guestY = ev.pageY
+
+   let patronX = guestX - patron2.getBoundingClientRect().left;
+   let patronY = guestY - patron2.getBoundingClientRect().top;
+   console.log('patronX:', patronX)
+   console.log('patronY:', patronY)
+
+   let patXst = parseInt(patron2.style.left)
+   let patYst = parseInt(patron2.style.top);
+   console.log('patXst:', patXst)
+   console.log('patYst:', patYst)
+
+   if ((patronX < 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst - step
+      console.log('Верхній лівий кут')
+   }
+   if ((patronX > 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst + step
+      console.log('Нижній правий кут')
+   }
+   if ((patronX > 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst + step
+      console.log('Верхній правий кут')
+   }
+   if ((patronX < 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst - step
+      console.log('Нижній лівий кут')
+   }
+
+   patronTop2 = guestX
+   patronLeft2 = guestY
+
+   patron2.style.top = guestX + 'px'
+   patron2.style.left = guestY + 'px'
+
+})
+
+// Початкова координата другого патрона
+let patronTop3 = 205
+let patronLeft3 = 805
+
+patron3.style.top = patronTop3 + 'px'
+patron3.style.left = patronLeft3 + 'px'
+// ---------------------------
+
+let step3 = 15;
+
+patron3.addEventListener('click', (ev) => {
+   let guestX = ev.pageX
+   let guestY = ev.pageY
+
+   let patronX = guestX - patron3.getBoundingClientRect().left;
+   let patronY = guestY - patron3.getBoundingClientRect().top;
+   console.log('patronX:', patronX)
+   console.log('patronY:', patronY)
+
+   let patXst = parseInt(patron3.style.left)
+   let patYst = parseInt(patron3.style.top);
+   console.log('patXst:', patXst)
+   console.log('patYst:', patYst)
+
+   if ((patronX < 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst - step
+      console.log('Верхній лівий кут')
+   }
+   if ((patronX > 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst + step
+      console.log('Нижній правий кут')
+   }
+   if ((patronX > 100) && (patronY < 100)) {
+      guestX = patYst - step
+      guestY = patXst + step
+      console.log('Верхній правий кут')
+   }
+   if ((patronX < 100) && (patronY > 100)) {
+      guestX = patYst + step
+      guestY = patXst - step
+      console.log('Нижній лівий кут')
+   }
+
+   patronTop3 = guestX
+   patronLeft3 = guestY
+
+   patron3.style.top = guestX + 'px'
+   patron3.style.left = guestY + 'px'
+
+})
+
+
